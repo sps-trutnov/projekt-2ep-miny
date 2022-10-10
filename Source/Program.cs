215bf6937f4a8046a3cf380b7de45f3329f9e01d
Jakub Šenkýř <senkyr@spstrutnov.cz>
2022-10-10 10:35:22 +0200

    Založení projektu

---
+++ Source/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Miny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.WriteLine(" ASCII Miny ");
+            Console.WriteLine(" ---------- ");
+            Console.WriteLine();
+
+            Console.WriteLine("Stiskněte libovolnou klávesu pro ukončení...");
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
