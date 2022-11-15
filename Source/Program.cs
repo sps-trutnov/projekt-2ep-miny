6d823a8119aa9fadc3d4da9b94ebdfa21925871b
Jakub Šenkýř <senkyr@spstrutnov.cz>
2022-11-15 10:39:39 +0100

    Kultura kódu

--- Source/Program.cs
+++ Source/Program.cs
@@ -24,18 +24,16 @@
         static int[,] MinovePole;
         static TypPolicka[,] Maska;
 
-        static int KurzorX = 5;
-        static int KurzorY = 6;
+        static int KurzorX = 0;
+        static int KurzorY = 0;
 
         static void Main(string[] args)
         {
             VygenerovatHerniPlochu();
-           
-            StavHry stavHry = StavHry.Bezi;
 
+            StavHry stavHry = StavHry.Bezi;
             while (stavHry == StavHry.Bezi)
             {
-
                 Console.WriteLine("y:" + KurzorY);
                 Console.WriteLine("x:" + KurzorX);
                 for (int y = 0; y < Maska.GetLength(0); y++)
@@ -48,14 +46,8 @@
                 }
                 Console.WriteLine();
 
-
-
-
-
-
-
                 stavHry = InterakceSUzivatelem();
-                if(stavHry == StavHry.Prohra)
+                if (stavHry == StavHry.Prohra)
                 {
                     Console.WriteLine("mapa");
                     for (int y = 0; y < Maska.GetLength(0); y++)
@@ -68,7 +60,7 @@
                     }
                     Console.WriteLine();
                     Console.WriteLine("odkrytá mapa");
-                    
+
                     for (int x = 0; x < MinovePole.GetLength(1); x++)
                     {
                         for (int y = 0; y < MinovePole.GetLength(0); y++)
@@ -95,7 +87,6 @@
                     Console.WriteLine("prohra");
                 }
 
-
                 if (stavHry == StavHry.Vyhra)
                 {
                     Console.WriteLine("mapa");
@@ -110,22 +101,21 @@
                     Console.WriteLine();
                     Console.WriteLine("Výhra");
                 }
-
             }
         }
 
         static void VygenerovatHerniPlochu()
         {
-            MinovePole = new int[SirkaHerniPlochy, VyskaHerniPlochy];
+            MinovePole = new int[SirkaHerniPlochy, VyskaHerniPlochy];
             Maska = new TypPolicka[SirkaHerniPlochy, VyskaHerniPlochy];
 
             Random nahoda = new Random();
             int z = 0;
-            while(z<10)
+            while (z < 10)
             {
                 int Random_x = nahoda.Next(MinovePole.GetLength(0));
                 int Random_y = nahoda.Next(MinovePole.GetLength(1));
-                if(MinovePole[Random_x, Random_y] != -1)
+                if (MinovePole[Random_x, Random_y] != -1)
                 {
                     MinovePole[Random_x, Random_y] = -1;
                     z++;
@@ -138,7 +128,6 @@
                 int x = a % VyskaHerniPlochy;
                 int y = a / SirkaHerniPlochy;
                 if (MinovePole[(x % SirkaHerniPlochy), y] != -1)
-                
                 {
                     if (y != 0 && y % VyskaHerniPlochy != VyskaHerniPlochy - 1)
                     {
@@ -240,7 +229,7 @@
             Console.Write("Stiskni klávesu");
             ConsoleKeyInfo klavesa = Console.ReadKey();
             Console.WriteLine();
-            
+
             if (klavesa.Key == ConsoleKey.RightArrow)
             {
                 Console.Clear();
@@ -249,8 +238,6 @@
                 {
                     KurzorX += 1;
                 }
-                
-
             }
 
             if (klavesa.Key == ConsoleKey.Escape)
@@ -266,7 +253,6 @@
                 {
                     KurzorX -= 1;
                 }
-                
             }
 
             if (klavesa.Key == ConsoleKey.DownArrow)
@@ -277,7 +263,6 @@
                 {
                     KurzorY += 1;
                 }
-                
             }
 
             if (klavesa.Key == ConsoleKey.UpArrow)
@@ -288,7 +273,6 @@
                 {
                     KurzorY -= 1;
                 }
-                
             }
 
             if (klavesa.Key == ConsoleKey.V)
@@ -299,53 +283,46 @@
                 {
                     Maska[KurzorY, KurzorX] = TypPolicka.Vlajka;
                 }
-
-
-                else if(Maska[KurzorY, KurzorX] == TypPolicka.Vlajka)
+                else if (Maska[KurzorY, KurzorX] == TypPolicka.Vlajka)
                 {
                     Maska[KurzorY, KurzorX] = TypPolicka.Zakryte;
                 }
-            }   
-            
+            }
+
             if (klavesa.Key == ConsoleKey.Enter)
             {
                 Console.Clear();
-                
+
                 if (Maska[KurzorY, KurzorX] == TypPolicka.Zakryte)
                 {
                     Maska[KurzorY, KurzorX] = TypPolicka.Odkryte;
                     if (MinovePole[KurzorY, KurzorX] == -1)
                     {
- 
+
                         return StavHry.Prohra;
                     }
                 }
-                
             }
             else
             {
                 Console.Clear();
             }
 
-            
             for (int x = 0; x < MinovePole.GetLength(1); x++)
             {
-               for (int y = 0; y < MinovePole.GetLength(0); y++)
-                {   
+                for (int y = 0; y < MinovePole.GetLength(0); y++)
+                {
                     if (MinovePole[y, x] == 0 && Maska[y, x] != TypPolicka.Odkryte)
                     {
                         return StavHry.Bezi;
                     }
-                    
-                    if (MinovePole[y, x] == -1 && Maska[y, x] != TypPolicka.Vlajka )
+
+                    if (MinovePole[y, x] == -1 && Maska[y, x] != TypPolicka.Vlajka)
                     {
-                      return StavHry.Bezi;
+                        return StavHry.Bezi;
                     }
                 }
             }
-
-            
-
 
             return StavHry.Vyhra;
         }
@@ -355,60 +332,49 @@
             Console.SetCursorPosition(0, 0);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.BackgroundColor = ConsoleColor.Green;
-
-                
         }
 
         static void ZobrazitHerniPlochu()
-        {
-            //Maska[3, 4] = TypPolicka.Vlajka;
-            //Maska[4, 5] = TypPolicka.Zakryte;
-             
-            for (int pocitacka_radku = 0; pocitacka_radku < VyskaHerniPlochy; pocitacka_radku++)
-            {
-             
-                Console.WriteLine("+-+-+-+-+-+-+-+-+-+");
-                
-                              
-                for (int pocitacka_pozic = 0; pocitacka_pozic < SirkaHerniPlochy; pocitacka_pozic++)
+        {
+            for (int pocitacka_radku = 0; pocitacka_radku < VyskaHerniPlochy; pocitacka_radku++)
+            {
+                Console.WriteLine("+-+-+-+-+-+-+-+-+-+");
+
+                for (int pocitacka_pozic = 0; pocitacka_pozic < SirkaHerniPlochy; pocitacka_pozic++)
                 {
                     Console.Write("|");
-                    if (KurzorX == pocitacka_pozic && KurzorY == pocitacka_radku)
-                    {
+
+                    if (KurzorX == pocitacka_pozic && KurzorY == pocitacka_radku)
+                    {
                         Console.BackgroundColor = ConsoleColor.Red;
                     }
-                    else
-                    {
+                    else
+                    {
                         Console.BackgroundColor = ConsoleColor.White;
                     }
 
-                    string znak = "nic";
-                    if (Maska[pocitacka_radku, pocitacka_pozic] == TypPolicka.Zakryte)
-                    {
-                        znak = "#";
-                    }
-                    else if (Maska[pocitacka_radku, pocitacka_pozic] == TypPolicka.Vlajka)
-                    {
-                        znak = "F";
+                    string znak;
+                    if (Maska[pocitacka_radku, pocitacka_pozic] == TypPolicka.Zakryte)
+                    {
+                        znak = "#";
                     }
-                    else if (MinovePole[pocitacka_radku, pocitacka_pozic] == -1)
-                    {
-                        znak = "M";
-                    }
-                    else 
-                    {
-                        znak = Convert.ToString(MinovePole[pocitacka_radku, pocitacka_pozic]);
+                    else if (Maska[pocitacka_radku, pocitacka_pozic] == TypPolicka.Vlajka)
+                    {
+                        znak = "F";
                     }
-                    
-
-                    Console.Write(znak);
-                    Console.BackgroundColor = ConsoleColor.White;
+                    else if (MinovePole[pocitacka_radku, pocitacka_pozic] == -1)
+                    {
+                        znak = "M";
+                    }
+                    else
+                    {
+                        znak = Convert.ToString(MinovePole[pocitacka_radku, pocitacka_pozic]);
+                    }
+                    Console.Write(znak);
+                    Console.BackgroundColor = ConsoleColor.White;
                 }
                 Console.Write("|");
-                Console.WriteLine();  
-                
-
-
+                Console.WriteLine();
             }
             Console.WriteLine("+-+-+-+-+-+-+-+-+-+");
         }
